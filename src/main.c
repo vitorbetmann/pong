c716062701364133a8638a272aaec4ff7b42b60e
Vitor Betmann <vitorbetmann@icloud.com>
2025-05-17 02:22:48 -0400

    Low Res Update - Virtual Screen Implementation

--- src/main.c
+++ src/main.c
@@ -1,22 +1,43 @@
 #include <raylib.h>
-#include <stdio.h>
-#include <inttypes.h>
+
+// Defines
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+#define V_WINDOW_WIDTH 432
+#define V_WINDOW_HEIGHT 243
+#define FONTSIZE 12
+// Prototypes
+// Variables
 
 int main(void)
 {
-    const int WINDOW_WIDTH = 1280;
-    const int WINDOW_HEIGHT = 720;
-
-    SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI);
+    SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
+    RenderTexture2D vScreen = LoadRenderTexture(V_WINDOW_WIDTH, V_WINDOW_HEIGHT);
     SetTargetFPS(60);
+    float scale = (float)WINDOW_WIDTH / V_WINDOW_WIDTH;
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
         char *greeting = "Hello Pong!";
+        int textWidth = MeasureText(greeting, FONTSIZE);
+        BeginTextureMode(vScreen);
         ClearBackground(BLACK);
-        DrawText(greeting, (WINDOW_WIDTH - TextLength(greeting)) / 2, WINDOW_HEIGHT / 2, 12, WHITE);
+        DrawText(greeting, (V_WINDOW_WIDTH - textWidth) / 2, (V_WINDOW_HEIGHT - FONTSIZE) / 2, FONTSIZE, WHITE);
+        EndTextureMode();
+
+        BeginDrawing();
+        DrawTexturePro(
+            vScreen.texture,
+            (Rectangle){0, 0, vScreen.texture.width, -vScreen.texture.height},
+            (Rectangle){0, 0, WINDOW_WIDTH, WINDOW_HEIGHT},
+            (Vector2){0, 0},
+            0,
+            WHITE);
         EndDrawing();
     }
+
+    UnloadRenderTexture(vScreen);
+    CloseWindow();
+    return 0;
 }
