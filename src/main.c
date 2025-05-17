87442bdccc5597e868a2fd175e74217a2bbaa616
vitorbetmann <vitorbetmann@me.com>
2025-05-16 22:18:55 -0400

    Day 0 Update

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
-#include "raylib.h"
+#include <raylib.h>
+#include <stdio.h>
+#include <inttypes.h>
 
 int main(void)
 {
-    InitWindow(800, 600, "Pong Test Window");
+    const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 720;
+
+    SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_HIGHDPI);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Pong is running!", 300, 280, 20, DARKGRAY);
+        char *greeting = "Hello Pong!";
+        ClearBackground(BLACK);
+        DrawText(greeting, (WINDOW_WIDTH - TextLength(greeting)) / 2, WINDOW_HEIGHT / 2, 12, WHITE);
         EndDrawing();
     }
-
-    CloseWindow();
-    return 0;
 }
