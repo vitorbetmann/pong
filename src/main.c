6ee119ed17163e2994174a72fa6a46514c9d2fab
Vitor Betmann <vitorbetmann@icloud.com>
2025-05-18 13:24:54 -0400

    The Rectangle Update

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
-// TODO: Draw Paddles and Ball
 // TODO: Line 43: Is SetTargetFPS() a good thing to use?
 
 #include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Defines
 #define WINDOW_WIDTH 1280
@@ -9,6 +10,9 @@
 #define V_WINDOW_WIDTH 432
 #define V_WINDOW_HEIGHT 243
 #define FONTSIZE 12
+#define PADDLE_WIDTH 5
+#define PADDLE_HEIGHT 20
+#define BALLSIZE 4
 
 // Variables
 RenderTexture2D vScreen;
@@ -19,6 +23,9 @@ void GameInit();
 void GameRun();
 void DrawAll();
 void DrawOnVScreen();
+void DrawPaddle(int posX, int posY);
+void DrawBall();
+void DrawGreeting();
 void DrawOnWindow();
 void GameUnload();
 
@@ -60,14 +67,32 @@ void DrawAll()
 void DrawOnVScreen()
 {
 
+    BeginTextureMode(vScreen);
+    DrawPaddle(10, 30);
+    DrawPaddle(V_WINDOW_WIDTH - 10, V_WINDOW_HEIGHT - 50);
+    DrawBall();
+    DrawGreeting();
+    EndTextureMode();
+}
+
+void DrawPaddle(int posX, int posY)
+{
+    DrawRectangle(posX, posY, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+}
+
+void DrawBall()
+{
+    DrawRectangle((V_WINDOW_WIDTH - BALLSIZE) / 2, (V_WINDOW_HEIGHT - BALLSIZE) / 2, BALLSIZE, BALLSIZE, WHITE);
+}
+
+void DrawGreeting()
+{
     char *greeting = "Hello Pong!";
     Vector2 textWidth = MeasureTextEx(font, greeting, FONTSIZE, 2);
     Vector2 textPosition = (Vector2){(V_WINDOW_WIDTH - textWidth.x) / 2,
-                                     (V_WINDOW_HEIGHT - textWidth.y) / 2};
-    BeginTextureMode(vScreen);
+                                     (V_WINDOW_HEIGHT - textWidth.y) * 0.06f};
     ClearBackground((Color){40, 45, 52, 255});
     DrawTextEx(font, greeting, textPosition, FONTSIZE, 2, WHITE);
-    EndTextureMode();
 }
 
 void DrawOnWindow()
