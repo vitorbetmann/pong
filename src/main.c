06691e0c109526c2cfaa693bb454bd082956fffe
Vitor Betmann <vitorbetmann@icloud.com>
2025-05-19 12:24:49 -0400

    The Paddle Update

--- src/main.c
+++ src/main.c
@@ -1,26 +1,38 @@
-// TODO: Line 43: Is SetTargetFPS() a good thing to use?
-
 #include <raylib.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 // Defines
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
-#define V_WINDOW_WIDTH 432
-#define V_WINDOW_HEIGHT 243
+#define TARGET_FPS 60
+#define V_WIDTH 432
+#define V_HEIGHT 243
 #define FONTSIZE 12
 #define PADDLE_WIDTH 5
 #define PADDLE_HEIGHT 20
+#define PADDLE_SPEED 200
 #define BALLSIZE 4
+
+// Data Types
+typedef enum
+{
+    PLAYER_1,
+    PLAYER_2,
+} PlayerNumber;
 
 // Variables
 RenderTexture2D vScreen;
 Font font;
+float dt;
+int p1Score, p2Score;
+int player1Y = 30, player2Y = V_HEIGHT - 50;
+const int PLAYER_1_X = 10, PLAYER_2_X = V_WIDTH - 10;
 
 // Prototypes
 void GameInit();
 void GameRun();
+void UpdateAll();
+void UpdatePaddle(PlayerNumber playerNum);
+void UpdateBall();
 void DrawAll();
 void DrawOnVScreen();
 void DrawPaddle(int posX, int posY);
@@ -37,6 +49,7 @@ int main(void)
     return 0;
 }
 
+// Functions
 void GameInit()
 {
     SetConfigFlags(
@@ -44,18 +57,50 @@ void GameInit()
         FLAG_VSYNC_HINT |
         FLAG_WINDOW_RESIZABLE);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
-    vScreen = LoadRenderTexture(V_WINDOW_WIDTH, V_WINDOW_HEIGHT);
+    vScreen = LoadRenderTexture(V_WIDTH, V_HEIGHT);
     font = LoadFont("../assets/pong_font.ttf");
     SetTextureFilter(font.texture, TEXTURE_FILTER_POINT);
-    SetTargetFPS(60);
+    SetTargetFPS(TARGET_FPS);
 }
 
 void GameRun()
 {
     while (!WindowShouldClose())
     {
+        UpdateAll();
         DrawAll();
     }
+}
+
+void UpdateAll()
+{
+    dt = GetFrameTime();
+    UpdatePaddle(PLAYER_1);
+    UpdatePaddle(PLAYER_2);
+    UpdateBall();
+}
+
+void UpdatePaddle(PlayerNumber playerNum)
+{
+    switch (playerNum)
+    {
+    case PLAYER_1:
+        if (IsKeyDown(KEY_W))
+            player1Y -= PADDLE_SPEED * dt;
+        else if (IsKeyDown(KEY_S))
+            player1Y += PADDLE_SPEED * dt;
+        return;
+    case PLAYER_2:
+        if (IsKeyDown(KEY_UP))
+            player2Y -= PADDLE_SPEED * dt;
+        else if (IsKeyDown(KEY_DOWN))
+            player2Y += PADDLE_SPEED * dt;
+        return;
+    }
+}
+
+void UpdateBall()
+{
 }
 
 void DrawAll()
@@ -66,10 +111,9 @@ void DrawAll()
 
 void DrawOnVScreen()
 {
-
     BeginTextureMode(vScreen);
-    DrawPaddle(10, 30);
-    DrawPaddle(V_WINDOW_WIDTH - 10, V_WINDOW_HEIGHT - 50);
+    DrawPaddle(PLAYER_1_X, player1Y);
+    DrawPaddle(PLAYER_2_X, player2Y);
     DrawBall();
     DrawGreeting();
     EndTextureMode();
@@ -82,15 +126,15 @@ void DrawPaddle(int posX, int posY)
 
 void DrawBall()
 {
-    DrawRectangle((V_WINDOW_WIDTH - BALLSIZE) / 2, (V_WINDOW_HEIGHT - BALLSIZE) / 2, BALLSIZE, BALLSIZE, WHITE);
+    DrawRectangle((V_WIDTH - BALLSIZE) / 2, (V_HEIGHT - BALLSIZE) / 2, BALLSIZE, BALLSIZE, WHITE);
 }
 
 void DrawGreeting()
 {
     char *greeting = "Hello Pong!";
     Vector2 textWidth = MeasureTextEx(font, greeting, FONTSIZE, 2);
-    Vector2 textPosition = (Vector2){(V_WINDOW_WIDTH - textWidth.x) / 2,
-                                     (V_WINDOW_HEIGHT - textWidth.y) * 0.06f};
+    Vector2 textPosition = (Vector2){(V_WIDTH - textWidth.x) / 2,
+                                     (V_HEIGHT - textWidth.y) * 0.06f};
     ClearBackground((Color){40, 45, 52, 255});
     DrawTextEx(font, greeting, textPosition, FONTSIZE, 2, WHITE);
 }
