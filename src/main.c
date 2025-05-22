9891ec43d7283223b02bf16ab98650b6f8c6c7ec
Vitor Betmann <vitorbetmann@icloud.com>
2025-05-22 02:28:08 -0400

    The FPS Update

--- src/main.c
+++ src/main.c
@@ -6,15 +6,13 @@
 #include <stdio.h>
 
 // Data Types
-typedef enum
-{
-    START,
-    PLAY,
+typedef enum {
+  START,
+  PLAY,
 } GameState;
-typedef enum
-{
-    PLAYER_1,
-    PLAYER_2,
+typedef enum {
+  PLAYER_1,
+  PLAYER_2,
 } PlayerNumber;
 
 // Variables
@@ -27,6 +25,7 @@ Font font;
 float dt;
 int p1Score, p2Score;
 Color const BACKGROUND = {40, 45, 52, 255};
+bool canDrawFPS = false;
 
 // Prototypes
 void GameInit();
@@ -35,134 +34,126 @@ void UpdateAll();
 void GetInput();
 void DrawAll();
 void ScoreDraw();
+void ToggleFPS();
+void DisplayFPS();
 void DrawOnVScreen();
 void GreetingDraw();
 void DrawOnWindow();
 void GameUnload();
 
 // Main
-int main(void)
-{
-    GameInit();
-    GameRun();
-    GameUnload();
-    return 0;
+int main(void) {
+  GameInit();
+  GameRun();
+  GameUnload();
+  return 0;
 }
 
 // Functions
-void GameInit()
-{
-    SetConfigFlags(
-        FLAG_FULLSCREEN_MODE |
-        FLAG_VSYNC_HINT |
-        FLAG_WINDOW_RESIZABLE);
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
-    vScreen = LoadRenderTexture(V_WIDTH, V_HEIGHT);
-    font = LoadFont("../assets/pong_font.ttf");
-    SetTextureFilter(font.texture, TEXTURE_FILTER_POINT);
-    SetTargetFPS(TARGET_FPS);
+void GameInit() {
+  SetConfigFlags(FLAG_FULLSCREEN_MODE | FLAG_VSYNC_HINT |
+                 FLAG_WINDOW_RESIZABLE);
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong");
+  vScreen = LoadRenderTexture(V_WIDTH, V_HEIGHT);
+  font = LoadFont("../assets/pong_font.ttf");
+  SetTextureFilter(font.texture, TEXTURE_FILTER_POINT);
+  HideCursor();
+  // SetTargetFPS(TARGET_FPS);
 
-    BallReset(&ball);
-    gameState = START;
+  BallReset(&ball);
+  gameState = START;
 }
 
-void GameRun()
-{
-    while (!WindowShouldClose())
-    {
-        switch (gameState)
-        {
-        case START:
-            if (IsKeyDown(KEY_ENTER))
-                gameState = PLAY;
-            break;
-        case PLAY:
-            UpdateAll();
-            break;
-        }
-        DrawAll();
+void GameRun() {
+  while (!WindowShouldClose()) {
+    switch (gameState) {
+    case START:
+      if (IsKeyDown(KEY_ENTER))
+        gameState = PLAY;
+      break;
+    case PLAY:
+      UpdateAll();
+      break;
     }
+    DrawAll();
+  }
 }
 
-void UpdateAll()
-{
-    dt = GetFrameTime();
-    GetInput();
-    BallUpdate(&ball, dt);
+void UpdateAll() {
+  dt = GetFrameTime();
+  GetInput();
+  BallUpdate(&ball, dt);
 }
 
-void GetInput()
-{
-    if (IsKeyDown(KEY_W))
-    {
-        PaddleMoveUp(&paddle1, dt);
-    }
-    else if (IsKeyDown(KEY_S))
-    {
-        PaddleMoveDown(&paddle1, dt);
-    }
-    if (IsKeyDown(KEY_UP))
-    {
-        PaddleMoveUp(&paddle2, dt);
-    }
-    else if (IsKeyDown(KEY_DOWN))
-    {
-        PaddleMoveDown(&paddle2, dt);
-    }
+void GetInput() {
+  if (IsKeyPressed(KEY_F)) {
+    ToggleFPS();
+  }
+  if (IsKeyDown(KEY_W)) {
+    PaddleMoveUp(&paddle1, dt);
+  } else if (IsKeyDown(KEY_S)) {
+    PaddleMoveDown(&paddle1, dt);
+  }
+  if (IsKeyDown(KEY_UP)) {
+    PaddleMoveUp(&paddle2, dt);
+  } else if (IsKeyDown(KEY_DOWN)) {
+    PaddleMoveDown(&paddle2, dt);
+  }
 }
 
-void DrawAll()
-{
-    DrawOnVScreen();
-    DrawOnWindow();
+void ToggleFPS() { canDrawFPS = !canDrawFPS; }
+
+void DisplayFPS() {
+  if (canDrawFPS) {
+    DrawFPS(10, 10);
+  }
 }
 
-void DrawOnVScreen()
-{
-    BeginTextureMode(vScreen);
-    ScoreDraw();
-    PaddleDraw(&paddle1);
-    PaddleDraw(&paddle2);
-    BallDraw(&ball);
-    GreetingDraw();
-    EndTextureMode();
+void DrawAll() {
+  DrawOnVScreen();
+  DrawOnWindow();
 }
 
-void ScoreDraw()
-{
-    char buffer[MAX_SCORE_DIGITS_AMOUNT + 1]; // +1 for '\0'
-    sprintf(buffer, "%d", p1Score);
-    DrawText(buffer, V_WIDTH / 2 - 50, V_HEIGHT / 3, SCORE_FONTSIZE, WHITE);
-    sprintf(buffer, "%d", p2Score);
-    DrawText(buffer, V_WIDTH / 2 + 30, V_HEIGHT / 3, SCORE_FONTSIZE, WHITE);
+void DrawOnVScreen() {
+  BeginTextureMode(vScreen);
+  DisplayFPS();
+  ScoreDraw();
+  PaddleDraw(&paddle1);
+  PaddleDraw(&paddle2);
+  BallDraw(&ball);
+  GreetingDraw();
+  EndTextureMode();
 }
 
-void GreetingDraw()
-{
-    char *greeting = "Hello Pong!";
-    Vector2 textSize = MeasureTextEx(font, greeting, GREETING_FONTSIZE, 2);
-    Vector2 textPosition = (Vector2){(V_WIDTH - textSize.x) / 2,
-                                     (V_HEIGHT - textSize.y) * 0.06f};
-    ClearBackground(BACKGROUND);
-    DrawTextEx(font, greeting, textPosition, GREETING_FONTSIZE, 2, WHITE);
+void ScoreDraw() {
+  char buffer[MAX_SCORE_DIGITS_AMOUNT + 1]; // +1 for '\0'
+  sprintf(buffer, "%d", p1Score);
+  DrawText(buffer, V_WIDTH / 2 - 50, V_HEIGHT / 3, SCORE_FONTSIZE, WHITE);
+  sprintf(buffer, "%d", p2Score);
+  DrawText(buffer, V_WIDTH / 2 + 30, V_HEIGHT / 3, SCORE_FONTSIZE, WHITE);
 }
 
-void DrawOnWindow()
-{
-    BeginDrawing();
-    DrawTexturePro(
-        vScreen.texture,
-        (Rectangle){0, 0, vScreen.texture.width, -vScreen.texture.height},
-        (Rectangle){0, 0, WINDOW_WIDTH, WINDOW_HEIGHT},
-        (Vector2){0, 0},
-        0,
-        WHITE);
-    EndDrawing();
+void GreetingDraw() {
+  char *greeting = "Hello Pong!";
+  Vector2 textSize = MeasureTextEx(font, greeting, GREETING_FONTSIZE, 2);
+  Vector2 textPosition =
+      (Vector2){(V_WIDTH - textSize.x) / 2, (V_HEIGHT - textSize.y) * 0.06f};
+  ClearBackground(BACKGROUND);
+  DrawTextEx(font, greeting, textPosition, GREETING_FONTSIZE, 2, WHITE);
 }
 
-void GameUnload()
-{
-    UnloadRenderTexture(vScreen);
-    UnloadFont(font);
-    CloseWindow();
+void DrawOnWindow() {
+  BeginDrawing();
+  DrawTexturePro(
+      vScreen.texture,
+      (Rectangle){0, 0, vScreen.texture.width, -vScreen.texture.height},
+      (Rectangle){0, 0, WINDOW_WIDTH, WINDOW_HEIGHT}, (Vector2){0, 0}, 0,
+      WHITE);
+  EndDrawing();
+}
+
+void GameUnload() {
+  UnloadRenderTexture(vScreen);
+  UnloadFont(font);
+  CloseWindow();
 }
